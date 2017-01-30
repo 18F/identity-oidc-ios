5063d5d1f027b9279671d8b8e5c113a4c099fed1
Zach Margolis <zachary.margolis@gsa.gov>
2017-01-30 10:50:42 -0500

    New Xcode project

---
+++ OpenIDConnectExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenIDConnectExample
+//
+//  Created by Zachary Margolis on 1/30/17.
+//  Copyright © 2017 GSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
